3e484d352cea1b7023b853f7811675138980021f
rongshuai <rongshuai@bytedance.com>
2020-12-11 17:27:43 +0800

    name update
    Change-Id: Ic4a07f4232f50b614617e23806d8fc86ecf0e746

--- iot-cloud-tool/iot-cloud-monitor/src/main/java/com/iot/monitor/IotMonitorApp.java
+++ iot-cloud-tool/iot-cloud-monitor/src/main/java/com/iot/monitor/IotMonitorApp.java
@@ -7,10 +7,10 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableAdminServer
 @SpringBootApplication
-public class AnanopsMonitorApp
+public class IotMonitorApp
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(AnanopsMonitorApp.class, args);
+        SpringApplication.run(IotMonitorApp.class, args);
     }
 }
